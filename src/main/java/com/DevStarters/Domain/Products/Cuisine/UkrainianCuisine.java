84da5dea46b27cd18cce816458b687378b20158c
atrik <patriot02faqq@gmail.com>
2017-07-18 12:07:16 +0300

    create domain classes

---
+++ src/main/java/com/DevStarters/Domain/Products/Cuisine/UkrainianCuisine.java
@@ -0,0 +1,6 @@
+package com.DevStarters.Domain.Products.Cuisine;
+
+import com.DevStarters.Domain.Products.Product;
+
+public class UkrainianCuisine extends Product {
+}
---
+++ src/test/java/com/DevStarters/Domain/UserTest.java
@@ -0,0 +1,10 @@
+package com.DevStarters.Domain;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by atrik on 18.07.2017.
+ */
+public class UserTest {
+
+}
