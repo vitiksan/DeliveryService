46d6665aec5015d255d1de95551c19275eb679cb
atrik <patriot02faqq@gmail.com>
2017-07-21 12:41:40 +0300

    implements burger and cakes

--- src/main/java/com/DevStarters/Domain/Products/Foods/Burger.java
+++ src/main/java/com/DevStarters/Domain/Products/Foods/Burger.java
@@ -1,10 +1,7 @@
 package com.DevStarters.Domain.Products.Foods;
 
-import com.DevStarters.Domain.Products.Product;
-
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.HashSet;
 
 public class Burger extends Food{
     public Burger() {
@@ -13,11 +10,11 @@ public class Burger extends Food{
     }
 
     public Burger(String name, double price, String vendor, Period periodOfValidity) {
-        super(name, price, vendor, periodOfValidity, new HashSet<Product>(), "American");
+        super(name, price, vendor, periodOfValidity, "American");
     }
 
     public Burger(String name, double price, String vendor, LocalDate productionDate, LocalDate expirationDate) {
-        super(name, price, vendor, productionDate, expirationDate, new HashSet<Product>(), "American");
+        super(name, price, vendor, productionDate, expirationDate, "American");
     }
 
 }
--- src/main/java/com/DevStarters/Domain/Products/Foods/Cakes.java
+++ src/main/java/com/DevStarters/Domain/Products/Foods/Cakes.java
@@ -13,10 +13,10 @@ public class Cakes extends Food{
     }
 
     public Cakes(String name, double price, String vendor, Period periodOfValidity) {
-        super(name, price, vendor, periodOfValidity, new HashSet<Product>(), "Italian");
+        super(name, price, vendor, periodOfValidity, "Italian");
     }
 
     public Cakes(String name, double price, String vendor, LocalDate productionDate, LocalDate expirationDate) {
-        super(name, price, vendor, productionDate, expirationDate, new HashSet<Product>(), "Italian");
+        super(name, price, vendor, productionDate, expirationDate, "Italian");
     }
 }
